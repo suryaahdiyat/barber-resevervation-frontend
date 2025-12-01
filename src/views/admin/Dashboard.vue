19edbe92d8413346c5f00a63f848765ac8b80ce5
suryaahdiyat <hackandersonwite@gmail.com>
2025-12-01 12:28:35 +0800

    dashboard admin finish, and progress barber

--- src/views/admin/Dashboard.vue
+++ src/views/admin/Dashboard.vue
@@ -1,71 +1,162 @@
 <template>
   <div class="max-w-md md:max-w-2xl lg:max-w-4xl w-full mx-auto">
     <!-- section absensi harian barber -->
-    <div>
-      <h1 class="my-2 text-xl font-semibold dark:text-slate-100 text-slate-950">Absensi Barber Hari Ini</h1>
-      <div v-if="paginatedBarbers.length > 0">
-        <table v-if="!loading" class="dark:bg-slate-600 bg-slate-200 rounded-sm text-[8px] sm:text-[12px] overflow-hidden text-center max-w-md md:max-w-2xl lg:max-w-4xl w-full mx-auto">
-          <thead class="dark:bg-gray-700 bg-gray-600 text-slate-100 border-b-2 dark:border-slate-100 border-slate-800">
-            <tr>
-              <th class="px-2 py-1">No</th>
-              <th class="px-2 py-1">Nama</th>
-              <!-- <th class="px-2 py-1">Status</th> -->
-              <th class="px-2 py-1">Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr
-              v-for="(barber, index) in paginatedBarbers"
-              :key="barber.id"
-              class="border-b-2 dark:border-b-slate-200 border-b-slate-300"
+    <div class="">
+      <div class="space-y-4 my-3">
+        <details
+          class="group border-s-4 border-gray-200 bg-gray-50 p-4 dark:border-gray-700 dark:bg-gray-800 [&_summary::-webkit-details-marker]:hidden"
+          open
+        >
+          <summary class="flex items-center justify-between gap-1.5 text-gray-900 dark:text-white">
+            <h2 class="font-medium mb-2">Absensi Barber</h2>   
+            <svg
+              class="size-5 shrink-0 transition-transform duration-300 group-open:-rotate-180 hover:cursor-pointer"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-              <td class="px-2 py-1 relative text-center">
-                {{ (currentPage - 1) * perPage + index + 1 }}
-              </td>
-              <td class="px-2 py-1">{{ barber.name }}</td>
-              <!-- <td class="px-2 py-1">
-                <span class="px-2 py-1 rounded text-white" :class="barber.is_present ? 'bg-green-500' : 'bg-red-500'">
-                  {{ barber.is_present ? 'Hadir' : 'Tidak Hadir' }}
-                </span>
-              </td> -->
-              <td class="px-2 py-1 text-center gap-1 md:gap-4 flex justify-center items-center">
-                <!-- <button 
-                  v-if="!barber.is_present"
-                  @click="markAsPresent(barber.id)" 
-                  title="Tandai Hadir" 
-                  class="bg-green-500 hover:bg-emerald-600 text-white px-3 py-1 rounded text-sm duration-75"
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+            </svg>
+          </summary>
+          <div v-if="paginatedBarbers.length > 0">
+            <table v-if="!loadingBarbers" class="dark:bg-slate-600 bg-slate-200 rounded-sm text-[8px] sm:text-[12px] overflow-hidden text-center max-w-md md:max-w-2xl lg:max-w-4xl w-full mx-auto">
+              <thead class="dark:bg-gray-700 bg-gray-600 text-slate-100 border-b-2 dark:border-slate-100 border-slate-800">
+                <tr>
+                  <th class="px-2 py-1">No</th>
+                  <th class="px-2 py-1">Nama</th>
+                  <th class="px-2 py-1">Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr
+                  v-for="(barber, index) in paginatedBarbers"
+                  :key="barber.id"
+                  class="border-b-2 dark:border-b-slate-200 border-b-slate-300"
                 >
-                  Hadir
-                </button>
-                <button 
-                  v-else
-                  @click="markAsAbsent(barber.id)" 
-                  title="Tandai Tidak Hadir" 
-                  class="bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded text-sm duration-75"
-                >
-                  Tidak Hadir
-                </button> -->
-                <button 
-                  @click="markAsPresent(barber.id)" 
-                  title="Tandai Hadir" 
-                  class="bg-red-500 hover:bg-rose-600 hover:cursor-pointer text-white px-3 py-1 rounded text-sm duration-75"
-                >
-                  Off
-                </button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <!-- Pagination -->
-        <Pagination
-          :currentPage="currentPage"
-          :totalPages="totalPages"
-          @change-page="handlePageChange"
-        />
+                  <td class="px-2 py-1 relative text-center">
+                    {{ (currentPageBarbers - 1) * perPageBarbers + index + 1 }}
+                  </td>
+                  <td class="px-2 py-1">{{ barber.name }}</td>
+                  <td class="px-2 py-1 text-center gap-1 md:gap-4 flex justify-center items-center">
+                    <button 
+                      @click="markAsPresent(barber.id)" 
+                      title="Tandai Hadir" 
+                      class="bg-red-500 hover:bg-rose-600 hover:cursor-pointer text-white px-3 py-1 rounded text-sm duration-75"
+                    >
+                      Off
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <!-- Pagination -->
+            <Pagination
+              :currentPage="currentPageBarbers"
+              :totalPages="totalPagesBarbers"
+              @change-page="handlePageChangeBarbers"
+            />
+          </div>
+          <p v-else class="w-full text-slate-400 text-center py-4">
+            {{ loadingBarbers ? 'Loading...' : 'Semua barber sudah absen hari ini! 🎉' }}
+          </p>
+        </details>
       </div>
-      <p v-else class="w-full text-slate-400 text-center py-4">
-        {{ loading ? 'Loading...' : 'Semua barber sudah absen hari ini! 🎉' }}
-      </p>
+      <div class="space-y-4 my-3">
+        <details
+          class="group border-s-4 border-gray-200 bg-gray-50 p-4 dark:border-gray-700 dark:bg-gray-800 [&_summary::-webkit-details-marker]:hidden"
+          open
+        >
+          <summary class="flex items-center justify-between gap-1.5 text-gray-900 dark:text-white">
+            <h2 class="font-medium mb-2">Reservasi Baru</h2>   
+            <svg
+              class="size-5 shrink-0 transition-transform duration-300 group-open:-rotate-180 hover:cursor-pointer"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+            </svg>
+          </summary>
+          <div v-if="paginatedReservs.length > 0">
+            <table v-if="!loadingReservs" class="dark:bg-slate-600 bg-slate-200 rounded-sm text-[8px] sm:text-[12px] overflow-hidden text-center max-w-md md:max-w-2xl lg:max-w-4xl w-full mx-auto">
+              <thead class="dark:bg-gray-700 bg-gray-600 text-slate-100 border-b-2 dark:border-slate-100 border-slate-800">
+                <tr>
+                  <th class="px-2 py-1">No</th>
+                  <th class="px-2 py-1">Nama Cust</th>
+                  <th class="px-2 py-1">Tanggal/ Jam</th>
+                  <th class="px-2 py-1">Status</th>
+                  <th class="px-2 py-1">Nama Layanan</th>
+                  <th class="px-2 py-1">Aksi</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr
+                  v-for="(r, index) in paginatedReservs"
+                  :key="r.id"
+                  class="border-b-2 dark:border-b-slate-200 border-b-slate-300"
+                >
+                  <td class="px-2 py-1 relative text-center">
+                    {{ (currentPageReservs - 1) * perPageReservs + index + 1 }}
+                  </td>
+                  <td class="px-2 py-1">{{ r.customer?.name ? r.customer.name : "-" }}</td>
+                  <td class="px-2 py-1">{{ formatDateTime(r.date, r.time)}}</td>
+                  <td class="px-2 py-1">{{ r.status }}</td>
+                  <td class="px-2 py-1">{{ r.service.name }}</td>
+                  <td>
+                    <button
+                      @click="$router.push(`/admin/reservations/${r.id}/payment`)"
+                      class="px-3 py-1 rounded text-sm underline hover:cursor-pointer duration-75 text-[8px] sm:text-[12px]"
+                    >edit payment
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <!-- Pagination -->
+            <Pagination
+              :currentPage="currentPageReservs"
+              :totalPages="totalPagesReservs"
+              @change-page="handlePageChangeReservs"
+            />
+          </div>
+          <p v-else class="w-full text-slate-400 text-center py-4">
+            {{ loadingReservs ? 'Loading...' : 'Tidak ada reservasi yang belum di konfirmasi hari ini' }}
+          </p>
+        </details>
+      </div>
+      <!-- <div class="space-y-4">
+        <details
+          class="group border-s-4 border-gray-200 bg-gray-50 p-4 dark:border-gray-700 dark:bg-gray-800 [&_summary::-webkit-details-marker]:hidden"
+          open
+        >
+          <summary class="flex items-center justify-between gap-1.5 text-gray-900 dark:text-white">
+            <h2 class="font-medium">Reservation</h2>
+      
+            <svg
+              class="size-5 shrink-0 transition-transform duration-300 group-open:-rotate-180 hover:cursor-pointer"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+            </svg>
+          </summary>
+      
+          <div class="text-gray-900 dark:text-white pt-3">
+            <p class="flex justify-between w-1/3 pt-1"><span class="font-semibold w-1/2">Service Name</span></p>
+            <p class="flex justify-between w-1/3 pt-1"><span class="font-semibold w-1/2">Status</span></p>
+            <p class="flex justify-between w-1/3 pt-1"><span class="font-semibold w-1/2">Tanggal</span></p>
+          </div>
+        </details>
+      </div> -->
+      <!-- <div v-else class="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
+        <dt class="font-medium text-gray-900 dark:text-white">Reservasi</dt>
+  
+        <dd class="text-gray-700 sm:col-span-2 dark:text-gray-200">Tidak ada data reservasi</dd>
+      </div> -->
     </div>
     <!-- reset absensi manual -->
     <button @click="handleResetAttendance()" class="bg-cyan-600 text-white w-full mt-2 py-2 rounded hover:cursor-pointer duration-75">
@@ -81,48 +172,85 @@
 import { ref, computed, onMounted, watch } from "vue";
 import Pagination from "../../components/Pagination.vue";
 import api from "@/api/axios";
+import dayjs from "dayjs";
+import 'dayjs/locale/id';
 
 const barbers = ref([]);
-const loading = ref(false);
-const showAll = ref(false); // Toggle untuk tampilkan semua atau hanya belum absen
+const reservations = ref([]);
+const loadingBarbers = ref(false);
+const loadingReservs = ref(false);
+const selectedStatus = ref("all");
+const selectedDate = ref(new Date().toISOString().split("T")[0]);
 
 //for the pagination
-const currentPage = ref(1);
-const perPage = ref(5);
+const currentPageBarbers  = ref(1);
+const perPageBarbers = ref(5);
+const currentPageReservs  = ref(1);
+const perPageReservs = ref(5);
 
 // Pagination logic
-const totalPages = computed(() =>
-  Math.ceil(barbers.value.length / perPage.value)
+const totalPagesBarbers = computed(() =>
+  Math.ceil(barbers.value.length / perPageBarbers.value)
+);
+// Pagination logic
+const totalPagesReservs = computed(() =>
+  Math.ceil(reservations.value.length / perPageReservs.value)
 );
 
 const paginatedBarbers = computed(() => {
-  const start = (currentPage.value - 1) * perPage.value;
-  const end = start + perPage.value;
+  const start = (currentPageBarbers.value - 1) * perPageBarbers.value;
+  const end = start + perPageBarbers.value;
   return barbers.value.slice(start, end);
 });
 
-const handlePageChange = (page) => {
-  currentPage.value = page;
+const paginatedReservs = computed(() => {
+  const start = (currentPageReservs.value - 1) * perPageReservs.value;
+  const end = start + perPageReservs.value;
+  return reservations.value.slice(start, end);
+});
+
+const handlePageChangeBarbers = (page) => {
+  currentPageBarbers.value = page;
+};
+
+const handlePageChangeReservs = (page) => {
+  currentPageReservs.value = page;
 };
 
 // Fetch barbers dengan query parameter
 const fetchBarbers = async() => {
-  loading.value = true;
+  loadingBarbers.value = true;
   try {
-    let endpoint = '/users/barbers';
-    
-    // Gunakan query parameter berdasarkan showAll
-    if (!showAll.value) {
-      endpoint += '?is_present=false'; // Hanya yang belum absen
-    }
-    
+    let endpoint = '/users/barbers?is_present=false';
+
     const res = await api.get(endpoint);
     barbers.value = res.data;
     console.log("Data barber:", barbers.value);
   } catch (err) {
     console.error("Error fetching barbers:", err);
   } finally {
-    loading.value = false;
+    loadingBarbers.value = false;
+  }
+};
+
+// Fetch Reservs dengan query parameter
+const fetchReservs = async() => {
+  loadingReservs.value = true;
+  try {
+    // let endpoint = ';
+
+    const res = await api.get(`/reservations`, {
+      params: {
+        status: "pending",
+        date: selectedDate.value,
+      },
+    });
+    reservations.value = res.data;
+    console.log("Data reservasi:", reservations.value);
+  } catch (err) {
+    console.error("Error fetching reservations:", err);
+  } finally {
+    loadingReservs.value = false;
   }
 };
 
@@ -142,23 +270,6 @@ const markAsPresent = async (barberId) => {
     alert("Gagal mengupdate status barber");
   }
 };
-
-// Tandai barber sebagai tidak hadir
-// const markAsAbsent = async (barberId) => {
-//   try {
-//     await api.patch(`/users/barbers/${barberId}/presence`, { 
-//       is_present: false 
-//     });
-    
-//     // Refresh data setelah update
-//     await fetchBarbers();
-    
-//     console.log(`Barber ${barberId} ditandai tidak hadir`);
-//   } catch (err) {
-//     console.error("Gagal update status:", err);
-//     alert("Gagal mengupdate status barber");
-//   }
-// };
 
 const handleResetAttendance = async () => {
   try {
@@ -183,11 +294,16 @@ const handleDailyBarberCheck = async () => {
   console.log("all barber reset")
 };
 
-// Watch perubahan showAll untuk refresh data
-watch(showAll, () => {
-  currentPage.value = 1; // Reset ke page 1 saat ganti filter
-  fetchBarbers();
-});
+const formatDateTime = (date, time) => {
+  if (!date || !time) return "-"
+  return dayjs(`${date}T${time}`).format('DD MMM YY, HH:mm')
+}
 
-onMounted(fetchBarbers);
+// Watch perubahan showAll untuk refresh data
+// watch(fetchBarbers);
+
+onMounted(() => {
+  fetchBarbers(),
+  fetchReservs()
+});
 </script>
--- src/views/barber/Dashboard.vue
+++ src/views/barber/Dashboard.vue
@@ -1,3 +1,120 @@
 <template>
-  <h1>Barber Dashboard</h1>
+  <div class="max-w-md md:max-w-2xl lg:max-w-4xl w-full mx-auto">
+    <div class="">
+      <div class="space-y-4 my-3">
+        <details
+          class="group border-s-4 border-gray-200 bg-gray-50 p-4 dark:border-gray-700 dark:bg-gray-800 [&_summary::-webkit-details-marker]:hidden"
+          open
+        >
+          <summary class="flex items-center justify-between gap-1.5 text-gray-900 dark:text-white">
+            <h2 class="font-medium mb-2">Absensi Barber</h2>   
+            <svg
+              class="size-5 shrink-0 transition-transform duration-300 group-open:-rotate-180 hover:cursor-pointer"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+            </svg>
+          </summary>
+          <div v-if="barber">
+            <table v-if="!loadingBarber" class="dark:bg-slate-600 bg-slate-200 rounded-sm text-[8px] sm:text-[12px] overflow-hidden text-center max-w-md md:max-w-2xl lg:max-w-4xl w-full mx-auto">
+              <thead class="dark:bg-gray-700 bg-gray-600 text-slate-100 border-b-2 dark:border-slate-100 border-slate-800">
+                <tr>
+                  <th class="px-2 py-1">Nama</th>
+                  <th class="px-2 py-1">Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr
+                  :key="barber.id"
+                  class="border-b-2 dark:border-b-slate-200 border-b-slate-300"
+                >
+                  <td class="px-2 py-1">{{ barber.name }}</td>
+                  <td class="px-2 py-1 text-center gap-1 md:gap-4 flex justify-center items-center">
+                    <button
+                      v-if="!barber.is_present"
+                      @click="handlePresent(barber.id)" 
+                      title="Tandai Hadir" 
+                      class="bg-rose-500 hover:bg-rose-600 hover:cursor-pointer text-white px-3 py-1 rounded text-sm duration-75"
+                    >
+                      Off
+                    </button>
+                    <button
+                      v-else
+                      @click="handlePresent(barber.id)" 
+                      title="Tandai Tidak Hadir" 
+                      class="bg-emerald-500 hover:bg-emerald-600 hover:cursor-pointer text-white px-3 py-1 rounded text-sm duration-75"
+                    >
+                      On
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </details>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script setup>
+import { ref, computed, onMounted } from "vue";
+import Pagination from "../../components/Pagination.vue";
+import api from "@/api/axios";
+import dayjs from "dayjs";
+import 'dayjs/locale/id';
+import { toast } from "vue3-toastify";
+
+const barber = ref([]);
+const loadingBarber = ref(false);
+const is_present = ref(false);
+
+// Fetch barbers dengan query parameter
+const fetchMyAttendance = async() => {
+  const userId = 3; // ganti dengan user ID nanti
+  loadingBarber.value = true;
+  try {
+    let endpoint = `/users/barber/presence/${userId}`;
+
+    const res = await api.get(endpoint);
+    barber.value = res.data;
+    is_present.value = barber.value.is_present;
+    // console.log("Data barber:", barber.value);
+    // toast.success("Berhasil absensi hadir")
+  } catch (err) {
+    console.error("Error fetching barber:", err);
+  } finally {
+    loadingBarber.value = false;
+  }
+};
+
+// Tandai barber sebagai hadir
+const handlePresent = async (barberId) => {
+  try {
+    await api.patch(`/users/barbers/${barberId}/presence`, { 
+      is_present: !is_present.value
+    });
+    
+    if(is_present.value) {
+      toast.success("Berhasil absensi tidak hadir")
+    }else {
+      toast.success("Berhasil absensi hadir")
+    }
+    // Refresh data setelah update
+    await fetchMyAttendance();
+  } catch (err) {
+    console.error("Gagal update status:", err);
+    alert("Gagal mengupdate status barber");
+  }
+};
+
+const formatDateTime = (date, time) => {
+  if (!date || !time) return "-"
+  return dayjs(`${date}T${time}`).format('DD MMM YY, HH:mm')
+};
+
+onMounted(fetchMyAttendance);
+</script>
